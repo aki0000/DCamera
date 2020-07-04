553439e49d2a4dd883911d415e62291cee68b925
aki0000 <akihisa.fromm1@gmail.com>
2020-07-04 11:45:07 +0900

    AVCappturePhotoの処理を実装

---
+++ DCamera/View/CapturePhotoView.swift
@@ -0,0 +1,30 @@
+//
+//  CapturePhotoView.swift
+//  DCamera
+//
+//  Created by Akihisa Hino on 2020/07/04.
+//  Copyright © 2020 Akihisa Hino. All rights reserved.
+//
+
+import SwiftUI
+
+struct CapturePhotoView: UIViewRepresentable {
+    
+    var capturePhotpViewModel = CapturePhotoViewModel()
+    
+    func makeUIView(context: Context) -> UIView {
+        let frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        return UIView(frame: frame)
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        self.capturePhotpViewModel.previewLayer.frame = uiView.frame        
+        uiView.layer.addSublayer(self.capturePhotpViewModel.previewLayer)
+    }
+}
+
+struct CapturePhotoView_Previews: PreviewProvider{
+    static var previews: some View {
+        CapturePhotoView()
+    }
+}
---
+++ DCamera/ViewModel/CapturePhotoViewModel.swift
@@ -0,0 +1,100 @@
+//
+//  CapturePhotoViewModel.swift
+//  DCamera
+//
+//  Created by Akihisa Hino on 2020/07/04.
+//  Copyright © 2020 Akihisa Hino. All rights reserved.
+//
+
+import SwiftUI
+import AVFoundation
+import UIKit
+
+class CapturePhotoViewModel: NSObject, ObservableObject {
+    
+    @Published var image: UIImage = UIImage()
+    
+    // Layer for preview
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    // Session
+    private let captureSession = AVCaptureSession()
+    // Be Taken a photo image
+    private var outputPhoto    = AVCapturePhotoOutput()
+    // Main Device
+    private var mainDevice:      AVCaptureDevice!
+    // InCamera Device
+    private var incameraDevice:  AVCaptureDevice!
+    // Current Device
+    private var currentDevice:   AVCaptureDevice!
+    
+    override init() {
+        super.init()
+        prepareCameraSession()
+        setUpDevice()
+        beginSession()
+        startSession()
+    }
+}
+    
+extension CapturePhotoViewModel: AVCapturePhotoCaptureDelegate{    
+    // Prepare a session for iPhone/iPad camera
+     private func prepareCameraSession() {
+         self.captureSession.sessionPreset = .photo
+     }
+     
+     // Set up an available Camera Device
+     private func setUpDevice() {
+         let deviceDiscocerySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: .video, position: .unspecified)
+         let devices = deviceDiscocerySession.devices
+         for device in devices {
+             if device.position == AVCaptureDevice.Position.back {
+                 self.mainDevice = device
+             } else if device.position == AVCaptureDevice.Position.back {
+                 self.incameraDevice = device
+             }
+         }
+         // Initiate Device is Main Device
+         self.currentDevice = self.mainDevice
+     }
+     
+     // Begin a session
+     private func beginSession() {
+         do {
+             let captureDeviceInput = try AVCaptureDeviceInput(device: self.currentDevice)
+             self.captureSession.addInput(captureDeviceInput)
+         } catch {
+             print(error.localizedDescription)
+         }
+         let previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+         previewLayer.connection?.videoOrientation = .portrait
+         self.previewLayer = previewLayer
+         self.previewLayer.connection?.videoOrientation = .portrait
+         self.outputPhoto = AVCapturePhotoOutput()
+         self.outputPhoto.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+         if captureSession.canAddOutput(self.outputPhoto) {
+             captureSession.addOutput(self.outputPhoto)
+         }
+     }
+     
+     // Start a session
+     func startSession() {
+         if self.captureSession.isRunning { return }
+         captureSession.startRunning()
+     }
+     
+     // End a session
+     func endSession() {
+         if !captureSession.isRunning { return }
+         captureSession.stopRunning()
+     }
+     
+     // Delegate when the use take a photo
+     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+         if let imageData = photo.fileDataRepresentation() {
+             if let uiImage = UIImage(data: imageData) {
+                 // Notifiy image value
+                 self.image  = uiImage
+             }
+         }
+     }
+}
