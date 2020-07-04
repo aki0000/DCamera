e5c52ea3ad2178d9db7b6dc80f2f9ce8eaabaefd
aki0000 <akihisa.fromm1@gmail.com>
2020-07-04 10:40:05 +0900

    Initial Commit

---
+++ DCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DCamera
+//
+//  Created by Akihisa Hino on 2020/07/04.
+//  Copyright © 2020 Akihisa Hino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
